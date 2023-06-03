f796559bd4dd90c20e8fb97095e870168d32cafe
origami-it-lab <135038469+origami-it-lab@users.noreply.github.com>
2023-06-03 10:52:11 +0530

    Update StopVMOnInactivity.ps1

--- StopVMOnInactivity.ps1
+++ StopVMOnInactivity.ps1
@@ -1,19 +1,25 @@
-   $idleThreshold = 10 # Inactivity threshold in minutes
-   $idleTime = 0
+[CmdletBinding()]
+Param()
 
-   while ($true) {
-       Start-Sleep -Seconds 60
-       $cpuUsage = (Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 1 -MaxSamples 5 |
-           Measure-Object -Property CounterSamples -Average).Average
+$idleThreshold = 10 # Inactivity threshold in minutes
+$idleTime = 0
 
-       if ($cpuUsage -lt 10) {
-           $idleTime += 1
-       } else {
-           $idleTime = 0
-       }
+while ($true) {
+    Start-Sleep -Seconds 60
+    $cpuUsage = (Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 1 -MaxSamples 5 |
+        Measure-Object -Property CounterSamples -Average).Average
 
-       if ($idleTime -ge $idleThreshold) {
-           az vm stop --name WindowsTen --resource-group FREE
-           break
-       }
-   }
+    if ($cpuUsage -lt 10) {
+        $idleTime += 1
+    } else {
+        $idleTime = 0
+    }
+
+    Write-Verbose "Idle time: $idleTime"
+    Write-Verbose "CPU usage: $cpuUsage"
+
+    if ($idleTime -ge $idleThreshold) {
+        az vm stop --name <vm-name> --resource-group <resource-group-name>
+        break
+    }
+}
